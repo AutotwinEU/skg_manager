3d8b65946509d5024857168643cd0ed68fdbe0a2
Ava-S <ava.swevels@gmail.com>
2025-01-20 17:51:47 +0100

    Update performance router

--- src/skg_manager/api/router/generic_routers/performance_router.py
+++ src/skg_manager/api/router/generic_routers/performance_router.py
@@ -1,20 +1,24 @@
+from typing import List
+
+from ....validation_and_calibration import EcdfMetricCalculatorInterface
 from ..interface_routers.performance_router_interface import PerformanceRouterInterface
 from ..router_result import Result
 
 
 class PerformanceRouter(PerformanceRouterInterface):
-    def __init__(self, performance_module):
-        self.performance_module = performance_module
+    def __init__(self, ecdf_metric_calculators: List[EcdfMetricCalculatorInterface]):
+        self.ecdf_services = ecdf_metric_calculators
 
-    def on_calculate_performance(self) -> Result:
-        try:
-            self.performance_module.remove_performance_artifacts()
-            self.performance_module.create_ecdf_collections()
-            self.performance_module.add_ecdf_collection_to_skg()
-
-            self.performance_module.create_aggregated_performance_html()
-            return Result(status=Result.Status.SUCCESS,
-                          message="Successfully created performance_library metrics available at "
-                                  "http://localhost:63342/skg_croma/docker/volumes/perfres/index.html")
-        except Exception as e:
-            return Result(status=Result.Status.FAILURE, message=str(e))
+    def on_calculate_performance(self, remove_previous_results=True) -> Result:
+        for ecdf_service in self.ecdf_services:
+            try:
+                if remove_previous_results:
+                    ecdf_service.remove_ecdfs_from_skg()
+                ecdf_service.calculate_ecdfs_from_skg()
+                ecdf_service.add_ecdfs_to_skg()
+                ecdf_service.create_aggregated_performance_html()
+            except Exception as e:
+                return Result(status=Result.Status.FAILURE, message=str(e))
+        return Result(status=Result.Status.SUCCESS,
+                      message="Successfully created performance_library metrics available at "
+                              "http://localhost:63342/skg_croma/docker/volumes/perfres/index.html")
