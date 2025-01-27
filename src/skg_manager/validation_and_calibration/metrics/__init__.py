54a26737a903893fa84d3e27dd197c6fc9707a2d
Ava-S <ava.swevels@gmail.com>
2025-01-27 12:08:16 +0100

    Add new Metrics to init files

--- src/skg_manager/validation_and_calibration/metrics/__init__.py
+++ src/skg_manager/validation_and_calibration/metrics/__init__.py
@@ -1,6 +1,11 @@
-from .metric_implementations import KolmogorovMetric, MedianRatioMetric, SimilarityMetric, WassersteinDistanceMetric
+from .metric_implementations import KolmogorovMetric, MedianRatioMetric, SimilarityMetric, WassersteinDistanceMetric, \
+    AverageDifferenceMetric, MedianDifferenceMetric, MaximumDifferenceMetric, MinimumDifferenceMetric
 
 __all__ = [KolmogorovMetric,
            MedianRatioMetric,
            SimilarityMetric,
-           WassersteinDistanceMetric]
+           WassersteinDistanceMetric,
+           AverageDifferenceMetric,
+           MedianDifferenceMetric,
+           MaximumDifferenceMetric,
+           MinimumDifferenceMetric]
