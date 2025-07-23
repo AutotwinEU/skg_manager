998724c5863ed4db04fbab0069293134cbc2ec31
Ava-S <ava.swevels@gmail.com>
2025-07-23 18:33:32 +0200

    Create a Metric Interface

--- src/skg_manager/validation_and_calibration/metrics/metric_interfaces/metric_interface.py
+++ src/skg_manager/validation_and_calibration/metrics/metric_interfaces/metric_interface.py
@@ -1,15 +1,66 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod, ABC
+from typing import List
+
+from promg import DatabaseConnection
+
+from ...measures.measure_interfaces import MeasureInterface
 
 
 class MetricInterface(ABC):
+    def __init__(self, name: str, measures: List[MeasureInterface]):
+        self.name = name
+        self.measures = measures
+        self.result = None
+        self.html_content = None
+        self.db_connection = None
+
+    ####################
+    # Abstract Methods #
+    ####################
     @abstractmethod
-    def calculate(self, gt_dist, sim_dist):
+    def clear_result(self):
         pass
 
     @abstractmethod
+    def calculate_result(self, start_time=None, end_time=None):
+        pass
+
+    @abstractmethod
+    def generate_html_content(self):
+        pass
+
+    #######################
+    # Implemented Methods #
+    #######################
+
+    def set_db_connection(self, db_connection: DatabaseConnection):
+        self.db_connection = db_connection
+
+    def check_db_connection(self):
+        if self.db_connection is None:
+            return False
+        return True
+
     def get_name(self):
-        pass
+        return self.name
 
-    @abstractmethod
-    def get_optimization_direction(self):
-        pass
+    def get_html_content(self):
+        if self.html_content is None:
+            self.generate_html_content()
+        return self.html_content
+
+    def get_dict_repr(self):
+        return {
+            'name': self.get_name(),
+            'content': self.get_html_content()
+        }
+
+    def get_measures(self):
+        result = []
+        for measure in self.measures:
+            measure = {
+                "name": measure.get_name(),
+                "optimization_type": measure.get_optimization_direction(),
+            }
+            result.append(measure)
+        return result
