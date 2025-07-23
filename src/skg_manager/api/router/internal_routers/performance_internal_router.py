79cf312def31ecbb085465a9b1fe139a1ba2ed0a
Ava-S <ava.swevels@gmail.com>
2025-07-23 18:35:23 +0200

    Replace metric by measure Provide API call to render the results as an html page

--- src/skg_manager/api/router/internal_routers/performance_internal_router.py
+++ src/skg_manager/api/router/internal_routers/performance_internal_router.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, send_from_directory, render_template, send_file
 
 from ...exceptions.exception_handler import db_exception_handler
 from ..interface_routers.performance_router_interface import PerformanceRouterInterface
@@ -13,15 +13,17 @@ class PerformanceInternalRouter:
         self.performance_routes.add_url_rule("/run", "handle_calculate_performance",
                                              view_func=self.handle_calculate_performance,
                                              methods=["POST"])
-        self.performance_routes.add_url_rule("/get_mean_metric_results", "handle_retrieve_metrics",
-                                             view_func=self.handle_retrieve_metrics,
+        self.performance_routes.add_url_rule("/get_mean", "handle_retrieve_mean",
+                                             view_func=self.handle_retrieve_mean,
                                              methods=["GET"])
         self.performance_routes.add_url_rule("/get_ecdf_types", "handle_get_ecdf_types",
                                              view_func=self.handle_get_ecdf_types,
                                              methods=["GET"])
-        self.performance_routes.add_url_rule("/get_metric_names", "handle_get_metric_names",
-                                             view_func=self.handle_get_metric_names,
+        self.performance_routes.add_url_rule("/get_measure_names", "handle_get_measure_names",
+                                             view_func=self.handle_get_measure_names,
                                              methods=["GET"])
+        self.performance_routes.add_url_rule("/show_results", "show_results",
+                                             view_func=self.show_results, methods=["GET"])
 
     @db_exception_handler
     def handle_calculate_performance(self) -> Response:
@@ -30,9 +32,9 @@ class PerformanceInternalRouter:
         return convert_result_into_response(result)
 
     @db_exception_handler
-    def handle_retrieve_metrics(self) -> Response:
+    def handle_retrieve_mean(self) -> Response:
         route_data = request.args
-        result = self.implementation.on_retrieve_metrics(route_data)
+        result = self.implementation.on_retrieve_mean(route_data)
         return convert_result_into_response(result)
 
     @db_exception_handler
@@ -41,7 +43,10 @@ class PerformanceInternalRouter:
         return convert_result_into_response(result)
 
     @db_exception_handler
-    def handle_get_metric_names(self) -> Response:
+    def handle_get_measure_names(self) -> Response:
         route_data = request.args
-        result = self.implementation.on_get_metric_names(route_data)
+        result = self.implementation.on_get_measure_names(route_data)
         return convert_result_into_response(result)
+
+    def show_results(self) -> str:
+        return self.implementation.on_show_results()
