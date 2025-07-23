81b1491bd74c1cb4e7395eb3f036f5eee3944a46
Ava-S <ava.swevels@gmail.com>
2025-07-23 18:34:42 +0200

    Replace metric by measure Update ValidationAndCalibrationService to be able to render its results as a html template Replace ECDF Wrappers by more generic MetricInterface

--- src/skg_manager/generic/services/calibration_validation_service.py
+++ src/skg_manager/generic/services/calibration_validation_service.py
@@ -1,59 +1,61 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 import pandas as pd
+from flask import send_from_directory, Response, render_template
 from promg import DatabaseConnection
 
 from ..service_interfaces.calibration_validation_interface import \
     ValidationAndCalibrationServiceInterface
 from ..queries.performance_queries import PerformanceQueryLibrary as pql
-from ...validation_and_calibration import EcdfWrapperInterface
+from ...validation_and_calibration import MetricInterface
 
 
 class ValidationAndCalibrationService(ValidationAndCalibrationServiceInterface):
-    def __init__(self, db_connection: DatabaseConnection, working_dir, ecdf_wrappers: List[EcdfWrapperInterface]):
+    def __init__(self, db_connection: DatabaseConnection, metrics: List[MetricInterface]):
         self.db_connection = db_connection
-        self.ecdf_wrappers = ecdf_wrappers
-        self.working_dir = working_dir
+        self.metrics = metrics
+        self.tabs = []
+
+    def render_validation_template(self) -> Response:
+        metrics = [metric.get_dict_repr() for metric in self.metrics]
+        return render_template('performance_results.html',
+                               metrics=metrics)
 
     def calculate_performance(self, start_date=None, end_date=None):
-        for ecdf_wrapper in self.ecdf_wrappers:
-            ecdf_wrapper.set_db_connection(self.db_connection)
-            ecdf_wrapper.remove_ecdfs_from_skg()
-            ecdf_wrapper.calculate_ecdfs_from_skg(start_time=start_date, end_time=end_date)
-            ecdf_wrapper.add_ecdfs_to_skg()
-            ecdf_wrapper.create_aggregated_performance_html(self.working_dir)
+        for metric in self.metrics:
+            metric.set_db_connection(self.db_connection)
+            metric.calculate_result(start_time=start_date, end_time=end_date)
 
-    def retrieve_mean_metrics(self, ecdf_type):
-        metric_results = self.db_connection.exec_query(
-            pql.get_get_metrics_query,
+    def retrieve_mean(self, ecdf_type):
+        measure_results = self.db_connection.exec_query(
+            pql.get_measures_query,
             **{"ecdf_type": ecdf_type}
         )
 
-        print(metric_results)
+        print(measure_results)
 
         pivot_results = {}
 
-        for metric_result in metric_results:
-            _type = metric_result["ecdf_type"]
-            metrics = metric_result["metrics"]
-            df_metrics = pd.DataFrame(metrics)
-            mean_df_metrics = df_metrics.mean()
-            mean_dict = mean_df_metrics.to_dict()
+        for measure_result in measure_results:
+            _type = measure_result["ecdf_type"]
+            measures = measure_result["measures"]
+            df_measures = pd.DataFrame(measures)
+            mean_df_measures = df_measures.mean()
+            mean_dict = mean_df_measures.to_dict()
             pivot_results[_type] = mean_dict
 
         return pivot_results
 
     def get_ecdf_types(self):
         ecdf_types = []
-        for ecdf_wrapper in self.ecdf_wrappers:
-            ecdf_types.append(ecdf_wrapper.get_ecdf_type())
+        for ecdf_wrapper in self.metrics:
+            ecdf_types.append(ecdf_wrapper.get_name())
         return ecdf_types
 
-    def get_metric_names(self, ecdf_type: Optional[str] = None):
-        metrics = {}
-        for ecdf_wrapper in self.ecdf_wrappers:
-            _type = ecdf_wrapper.get_ecdf_type()
-            if ecdf_type is None or ecdf_type == _type:
-                metrics[_type] = ecdf_wrapper.get_metrics()
-        return metrics
+    def get_measure_names(self, metric_name: Optional[str] = None):
+        measures = {}
+        for metric in self.metrics:
+            if metric_name is None or metric_name == metric.get_name():
+                measures[metric.get_name()] = metric.get_measures()
+        return measures
